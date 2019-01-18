ca31c42c4ffa3943d86d10142cbfa8171fc9c2f0
lizhe <lizhe052@126.com>
2019-01-18 13:41:44 +0800

    update edit file function.

--- app/src/main/java/com/termux/custom/edit/EditFragment.java
+++ app/src/main/java/com/termux/custom/edit/EditFragment.java
@@ -5,13 +5,18 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.databinding.library.baseAdapters.BR;
 import com.termux.R;
+import com.termux.custom.base.BaseAdapter;
+import com.termux.custom.bean.Command;
 import com.termux.custom.file.FileActivity;
 import com.termux.custom.file.FileFragment;
 import com.termux.custom.utils.LocalDataRepository;
@@ -19,6 +24,7 @@ import com.termux.custom.utils.StringUtils;
 import com.termux.databinding.EditFragmentBinding;
 
 import java.io.File;
+import java.util.ArrayList;
 
 
 /**
@@ -29,7 +35,6 @@ public class EditFragment extends Fragment implements View.OnClickListener {
 
     public static final int FOR_PATH_RESULT = 1000;
     private final String FUNCTION_EDIT_PATH = "FUNCTION_EDIT_PATH";
-
     private EditFragmentBinding binding;
 
     public static EditFragment newInstance() {
@@ -47,32 +52,94 @@ public class EditFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        binding.selectBut.setOnClickListener(this);
-        binding.saveBut.setOnClickListener(this);
         String cachePath = getCachePath();
         initPath(cachePath);
+
+        int id = 0;
+        ArrayList<Command> commands = new ArrayList<>();
+        ;
+        commands.add(new Command(++id, "SAVE", ""));
+        commands.add(new Command(++id, "SELECT", ""));
+        commands.add(new Command(++id, "init page", "---\n" +
+            "layout: default\n" +
+            "title: \"\"\n" +
+            "---\n" +
+            "\n" +
+            "<h2>{{ page.title }}</h2>\n" +
+            "\n" +
+            "<p class=\"publish_date\">31 Jan 2019</p>"));
+        commands.add(new Command(++id, "BR", "<br/>"));
+        commands.add(new Command(++id, "P", "<P></P>"));
+        commands.add(new Command(++id, "h3", "<h3></h3>"));
+        commands.add(new Command(++id, "h4", "<h4></h4>"));
+        commands.add(new Command(++id, "strong", "<strong></strong>"));
+        commands.add(new Command(++id, "pre", "<pre></pre>"));
+        commands.add(new Command(++id, "blockquote", "<blockquote></blockquote>"));
+        commands.add(new Command(++id, "font", "<font class=\"green\"></font>"));
+        commands.add(new Command(++id, "<>", "&lt;&gt;"));
+        commands.add(new Command(++id, "link", "<a target=\"_blank\" href=\"https://\">\n" +
+            "            https://</a><br/> "));
+        commands.add(new Command(++id, "image", "<div>\n" +
+            "  <img src=\"/images/blogs/idea/file_name/image.png\" width=\"512\" alt=\"\"/>\n" +
+            "  </div>"));
+
+        commands.add(new Command(++id, "audio", "<audio src=\"/audio/blogs/idea/file_name/20181104_001.m4a\"" +
+            " controls=\"controls\">\n" +
+            "Your browser does not support the audio element.\n" +
+            "</audio>"));
+
+        commands.add(new Command(++id, "video", "<video src=\"/video/blogs/idea/file_name/20181104_001.m4a\"" +
+            " controls=\"controls\">\n" +
+            "Your browser does not support the video element.\n" +
+            "</video>"));
+
+        BaseAdapter<Command> adapter = new BaseAdapter<>(R.layout.edit_item_view, BR.command, commands);
+        adapter.setListener(this);
+        binding.commandList.setAdapter(adapter);
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.selectBut:
-                Intent intent = new Intent(getContext(), FileActivity.class);
-                intent.putExtra(FileActivity.TAG_PATH_FILE, binding.addressET.getText().toString());
-                startActivityForResult(intent, FOR_PATH_RESULT);
-                break;
-            case R.id.saveBut:
-                String address = binding.addressET.getText().toString();
-                File file = new File(address);
-                if (!file.exists() || !file.isFile()) {
-                    Toast.makeText(getContext(), "Path is error!", Toast.LENGTH_LONG).show();
-                } else {
-                    String string = binding.textET.getText().toString();
-                    StringUtils.writeStringToFile(string, file.getParentFile().getAbsolutePath(), file.getName());
-                    Toast.makeText(getContext(), "DONE!", Toast.LENGTH_SHORT).show();
-                    savePath(address);
-                }
-                break;
+        Command command = null;
+        Object tag = v.getTag();
+        if (tag instanceof Command) {
+            command = (Command) tag;
+        }
+        if (command != null) {
+            switch (command.getId()) {
+                case 1://save
+                    String address = binding.addressET.getText().toString();
+                    File file = new File(address);
+                    if (!file.exists() || !file.isFile()) {
+                        Toast.makeText(getContext(), "Path is error!", Toast.LENGTH_LONG).show();
+                    } else {
+                        String string = binding.textET.getText().toString();
+                        StringUtils.writeStringToFile(string, file.getParentFile().getAbsolutePath(), file.getName());
+                        Toast.makeText(getContext(), "DONE!", Toast.LENGTH_SHORT).show();
+                        savePath(address);
+                    }
+                    break;
+                case 2://select
+                    Intent intent = new Intent(getContext(), FileActivity.class);
+                    intent.putExtra(FileActivity.TAG_PATH_FILE, binding.addressET.getText().toString());
+                    startActivityForResult(intent, FOR_PATH_RESULT);
+                    break;
+                default:
+                    EditText mEditText = binding.textET;//EditText对象
+                    int index = mEditText.getSelectionStart();//获取光标所在位置
+                    Editable edit = mEditText.getEditableText();//获取EditText的文字
+                    if (index < 0 || index >= edit.length()) {
+                        edit.append(command.getCommand());
+                    } else {
+                        edit.insert(index, command.getCommand());//光标所在位置插入文字
+                    }
+                    int position = command.getCommand().indexOf("><");
+                    if (position != -1) {
+                        mEditText.setSelection(index + position + 1);
+                    }
+                    break;
+            }
+
         }
     }
 
@@ -97,7 +164,7 @@ public class EditFragment extends Fragment implements View.OnClickListener {
             File file = new File(string);
             if (file.exists() && file.isFile()) {
                 binding.textET.setText(StringUtils.readToString(file));
-            }else {
+            } else {
                 binding.textET.setText("");
             }
         }
